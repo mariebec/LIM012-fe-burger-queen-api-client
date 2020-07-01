e41b1385e5376cb94b511cffaa9ed26e15640bb3
mariebec <jmariebc@gmail.com>
2020-07-01 16:23:01 -0500

    Corrigiendo conflictos con eslint

--- burger-queen/src/components/login/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import LoginView from './LogIn';
-
-const root = document.getElementById('root');
-
-ReactDOM.render(<LoginView />, root);
