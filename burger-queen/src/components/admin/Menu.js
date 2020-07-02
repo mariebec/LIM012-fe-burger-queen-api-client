c26c0cf63f98d26772c7aeda93fc7b094e42c8c7
Lourdes0896 <sam.lourdesmerino@gmail.com>
2020-07-02 17:05:01 -0500

    probando vista de menu

---
+++ burger-queen/src/components/admin/Menu.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import imgh from '../../assets/logo-fondomarron.png';
+
+const MenuView = () => (
+    <section className="container-menu">
+        <div className="header-menu">
+        <img src={imgh} alt="logo" className="logo-Header" />
+            <p>TOMAR PEDIDOS</p>
+            <div></div>
+        </div>
+        <div></div>
+
+    </section>
+
+)
+    
+
+export default MenuView;
