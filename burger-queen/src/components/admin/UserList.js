092fc299dbd99a0052f231ddcf90396a3c0154b7
mariebec <jmariebc@gmail.com>
2020-07-03 11:42:27 -0500

    Creando componente UserList

---
+++ burger-queen/src/components/admin/UserList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../Header';
+
+const UserList = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <button>Agregar usuario</button>
+        <p>Lista de usuarios</p>
+      </main>
+    </>
+  )
+}
+
+export default UserList;
