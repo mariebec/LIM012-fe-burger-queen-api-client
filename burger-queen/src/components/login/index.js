1ae79ddb32b0df65698242320d17a31982bc4b1a
Lourdes0896 <sam.lourdesmerino@gmail.com>
2020-07-04 17:44:57 -0500

    comentando index

--- burger-queen/src/components/login/index.js
+++ burger-queen/src/components/login/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import LoginView from './LogIn';
-import MenuView from '../admin/Menu';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import LoginView from './LogIn';
+// import MenuView from '../admin/Menu';
 
-const root = document.getElementById('root');
+// const root = document.getElementById('root');
 
-// ReactDOM.render(<LoginView />, root);
-ReactDOM.render(<MenuView />, root);
+// // ReactDOM.render(<LoginView />, root);
+// ReactDOM.render(<MenuView />, root);
