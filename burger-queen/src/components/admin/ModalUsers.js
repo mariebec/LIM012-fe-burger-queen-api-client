73fb9f08f96d3ebd10ef6fba9146d6338827ddc3
Lourdes0896 <sam.lourdesmerino@gmail.com>
2020-07-07 11:45:41 -0500

    editando modal user

--- burger-queen/src/components/admin/ModalUsers.js
+++ burger-queen/src/components/admin/ModalUsers.js
@@ -1,27 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const ModalUsers = () => {
-  console.log('Componente modal')
+  const [user, setUser] = useState ({
+    email: "",
+    password:"",
+    roles: {option:false}
+  })
+// 
+
+// const enviarDatos = (event) => {
+//   event.preventDefault();
+//   console.log(user.email)
+
+// }
+
+
   return (
     <section>
       <div>
         <p>Agregar usuario</p>
-        <form>
-          <label for="input-email">E-MAIL:</label>
-          <input id ="input-email" placeholder="Ingrese un e-mail"></input>
-          <label for="input-password">CLAVE:</label>
-          <input id ="input-password" placeholder="Ingrese la contraseña"></input>
-          <label for="input-admin">ADMIN:</label>
-          <select id ="input-admin">
-            <option>SI</option>
-            <option>NO</option>
+        <form className="form-user" >
+          <label htmlFor="input-email">E-MAIL:</label>
+          <input id ="input-email" placeholder="Ingrese un e-mail" name="email" type="e-mail" /> 
+          <label htmlFor="input-password">CLAVE:</label>
+          <input id ="input-password" placeholder="Ingrese la contraseña" name="password" type="password" />
+          <label htmlFor="input-admin">ADMIN:</label>
+          <select id ="input-admin" value={setUser.roles}>
+            <option value="false">NO</option>
+            <option value="true">SI</option>
           </select>
-          <label for="input-role">ROL:</label>
-          <select id ="input-role">
-            <option>Meserx</option>
-            <option>Cocinerx</option>
-            <option>Administradora</option>
-          </select>
+          <button type="button" className="btn-cancelUser">Cancelar</button>
+          <button type="submit" className="btn-newUser">Guardar</button>
         </form>
       </div>
     </section>
