f4bce139d877a583557d589c93c888ca2f504aba
Lourdes0896 <sam.lourdesmerino@gmail.com>
2020-07-07 08:41:55 -0500

    creando formulario para agrgar usuarios

---
+++ burger-queen/src/components/admin/ModalUser.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ModalUser = () => (
+  <div>
+    <p>agregar usuario</p>
+    <div>
+    <label>E-mail</label>
+    <input placeholder="Usuario" className="user" />
+      
+    </div>
+  
+  </div>
+
+
+)
+
+export default ModalUser;
