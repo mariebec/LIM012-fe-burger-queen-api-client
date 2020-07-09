9810f047241e1fd4abd623a52a079ddaca4404d8
mariebec <jmariebc@gmail.com>
2020-07-09 18:27:53 -0500

    Enviando props de funciones

--- burger-queen/src/components/admin/FormUsers.jsx
+++ burger-queen/src/components/admin/FormUsers.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const FormUsers = ({ user }) => {
+const FormUsers = ({ handleInputChange, handleSelectChange, handleSave }) => {
   return (
     <form className="form-modal" >
       <div className="form-container">
