05dd1e61981c94ffc342f6c926b1266e3a302f07
mariebec <jmariebc@gmail.com>
2020-07-08 21:05:50 -0500

    Eliminado ul
    Se cambió por tabla

--- burger-queen/src/components/admin/UserList.jsx
+++ burger-queen/src/components/admin/UserList.jsx
@@ -35,7 +35,7 @@ const UserList = () => {
 
   return (
     <>
-      <Header />
+      <Header title="ADMINISTRADOR" />
       <main className="container-users">
         <div className="btn-container">
           <button onClick={handleAddUser}>Agregar usuario</button>
@@ -70,31 +70,7 @@ const UserList = () => {
               )
             }
           </tbody>
-        </table>
-        {/* <ul>
-          <li className="table header">
-            <p className="l-column">Nombre</p>
-            <p className="m-column">Usuario</p>
-            <p className="xl-column">e-mail</p>
-            <p className="m-column">Rol</p>
-            <p className="s-column">Admin</p>
-            <p className="m-column">Clave</p>
-            <p className="m-column"></p>
-          </li>
-          <li className="table data">
-            <p className="l-column">Sandra Zapata</p>
-            <p className="m-column">szapata</p>
-            <p className="xl-column">szapata@bqueen.com</p>
-            <p className="m-column">Manager</p>
-            <p className="s-column">Si</p>
-            <p className="m-column">09867t</p>
-            <div className="m-column">
-              <i className="icon edit fas fa-edit"></i>
-              <i className="icon delete fas fa-trash-alt"></i>
-            </div>
-          </li>
-        </ul> */}
-        
+        </table>    
       </main>
     </>
   )
