1039fe96b1f12c6bf674f243012f455f6f8ca74a
mariebec <jmariebc@gmail.com>
2020-07-09 18:37:37 -0500

    Archivo de fotos

---
+++ burger-queen/src/components/order/product-img.js
@@ -0,0 +1,17 @@
+import hsimple from '../../assets/hamburguesa-simple.png';
+import hdouble from '../../assets/hamburguesa-doble.png';
+import fries from '../../assets/papas-fritas.png';
+import onion from '../../assets/aros-cebolla.png';
+import water500 from '../../assets/agua.png';
+import water750 from '../../assets/agua-big.png';
+
+const photos = {
+  hsimple,
+  hdouble,
+  fries,
+  onion,
+  water500,
+  water750,
+}
+
+export default photos;
