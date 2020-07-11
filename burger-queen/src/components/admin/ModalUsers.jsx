5800f395e89a31f74d27b659c8e830d4e8a8c6cd
mariebec <jmariebc@gmail.com>
2020-07-11 16:46:36 -0500

    Modularizando código
    Metodo fetch se separó en un archivo de controller

--- burger-queen/src/components/admin/ModalUsers.jsx
+++ burger-queen/src/components/admin/ModalUsers.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import FormUsers from './FormUsers';
+import { postUser } from '../../controller/admin-users';
 
 const ModalUsers = () => {
   // const idGenerado = (Math.random() * 100).toString();
@@ -12,40 +13,30 @@ const ModalUsers = () => {
 
   const [user, setUser] = useState(initialState);
 
-const handleInputChange = (e) => {
-  setUser({...user, [e.target.name]: e.target.value});
-} 
+  const handleInputChange = (e) => {
+    setUser({...user, [e.target.name]: e.target.value});
+  } 
 
-const handleSelectChange = (e) =>{
-  setUser({...user, roles: { admin: e.target.value === "SI" ? true : false }})
-}
+  const handleSelectChange = (e) =>{
+    setUser({...user, roles: { admin: e.target.value === "SI" ? true : false }})
+  }
 
-const handleCancel = () => {
-  setUser({...initialState});
-};
+  const handleCancel = () => {
+    setUser({...initialState});
+  };
 
   const handleSave = () => {
     // e.preventDefault();
     const userEmail = user.email.trim();
     const exRegEmail = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/;
     const userPassword = user.password.trim();
-    // const idGenerado = (Math.random() * 100).toString();
-    // console.log(idGenerado);
-    // setUser({...user, id: idGenerado})
   
     if (userEmail === '' || !exRegEmail.test(userEmail)) {
-      console.log('email invalido');
+      console.log('email inválido');
     } else if (userPassword === '' || userPassword.length < 6) {
-      console.log('contraseña invalida');
+      console.log('password inválido');
     } else {
-      fetch('http://localhost:3002/users', {
-        method: 'POST',
-        body: JSON.stringify(user),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
-        .then((resp) => resp.json())
+      postUser(user)
         .catch((error) => console.log(error))
         .then((resp) => console.log(resp));
     }
@@ -57,7 +48,12 @@ const handleCancel = () => {
       <section className="bg-img-fries">
         <div className="modal-window">
           <p className="title-modal">Agregar usuario</p>
-          <FormUsers user={user} handleInputChange={handleInputChange} handleSelectChange={handleSelectChange} handleCancel={handleCancel} handleSave={handleSave}/>
+          <FormUsers 
+            user={user} 
+            handleInputChange={handleInputChange} 
+            handleSelectChange={handleSelectChange} 
+            handleCancel={handleCancel} 
+            handleSave={handleSave}/>
         </div>
       </section>
     </>
