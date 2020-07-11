2ccfca51e50af45a21175ce308f2649241b73421
mariebec <marielabenitezc9@gmail.com>
2020-07-10 20:20:37 -0500

    Limpiando formulario
    Se pasa el estado de los campos, en los botones guardar y cancelar se reinicia el estado

--- burger-queen/src/components/admin/FormUsers.jsx
+++ burger-queen/src/components/admin/FormUsers.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 
-const FormUsers = ({ handleInputChange, handleSelectChange, handleSave }) => {
+const FormUsers = ({ user, handleInputChange, handleSelectChange, handleCancel, handleSave }) => {
   return (
-    <form className="form-modal" >
+    <form className="form-modal">
       <div className="form-container">
         <div>
           <label htmlFor="input-email" className="label-text">E-MAIL:</label>
           <input 
+            value={user.email}
             id ="input-email" 
             placeholder="Ingrese un e-mail" 
             name="email" type="email" 
@@ -16,6 +17,7 @@ const FormUsers = ({ handleInputChange, handleSelectChange, handleSave }) => {
         <div>
           <label htmlFor="input-password" className="label-text">CLAVE:</label>
           <input 
+            value={user.password}
             id ="input-password" 
             placeholder="Ingrese la contraseña" 
             name="password" type="password"
@@ -25,15 +27,15 @@ const FormUsers = ({ handleInputChange, handleSelectChange, handleSave }) => {
         <div>
           <label htmlFor="input-admin" className="label-text">ADMIN:</label>
           <div className="box-select">
-            <select id ="input-admin" onChange={handleSelectChange} className="select-modal">
+            <select id ="input-admin" onChange={handleSelectChange} className="select-modal" value={user.roles.admin ? "SI" : "NO"}>
               <option value="NO">NO</option>
               <option value="SI">SI</option>
             </select>
           </div>
         </div>
         <div>
-          <button type="button" className="btn-modal cancel">Cancelar</button>
-          <button type="submit" className="btn-modal save" onClick={handleSave}>Guardar</button>
+          <button type="button" className="btn-modal cancel" onClick={handleCancel}>Cancelar</button>
+          <button type="button" className="btn-modal save" onClick={handleSave}>Guardar</button>
         </div>
       </div>
     </form>
--- burger-queen/src/components/admin/ModalUsers.jsx
+++ burger-queen/src/components/admin/ModalUsers.jsx
@@ -2,13 +2,15 @@ import React, {useState} from 'react';
 import FormUsers from './FormUsers';
 
 const ModalUsers = () => {
-  const idGenerado = (Math.random() * 100).toString();
-  const [user, setUser] = useState ({
-    id: idGenerado,
-    email: "",
-    password:"",
+  // const idGenerado = (Math.random() * 100).toString();
+  const initialState = {
+    // id: idGenerado,
+    email:'',
+    password:'',
     roles: { admin: false }
-  })
+  };
+
+  const [user, setUser] = useState(initialState);
 
 const handleInputChange = (e) => {
   setUser({...user, [e.target.name]: e.target.value});
@@ -18,41 +20,44 @@ const handleSelectChange = (e) =>{
   setUser({...user, roles: { admin: e.target.value === "SI" ? true : false }})
 }
 
-
-const handleSave = (event) => {
-  event.preventDefault();
-  const userEmail = user.email.trim();
-  const exRegEmail = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/;
-  const userPassword = user.password.trim();
-  // const idGenerado = (Math.random() * 100).toString();
-  // console.log(idGenerado);
-  // setUser({...user, id: idGenerado})
-
-  if (userEmail === '' || !exRegEmail.test(userEmail)) {
-    console.log('email invalido');
-  } else if (userPassword === '' || userPassword.length < 6) {
-    console.log('contraseña invalida');
-  } else {
-    fetch('http://localhost:3002/users', {
-      method: 'POST',
-      body: JSON.stringify(user),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-      .then((resp) => resp.json())
-      .catch((error) => console.log(error))
-      .then((resp) => console.log(resp));
-  }
+const handleCancel = () => {
+  setUser({...initialState});
 };
 
+  const handleSave = () => {
+    // e.preventDefault();
+    const userEmail = user.email.trim();
+    const exRegEmail = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/;
+    const userPassword = user.password.trim();
+    // const idGenerado = (Math.random() * 100).toString();
+    // console.log(idGenerado);
+    // setUser({...user, id: idGenerado})
+  
+    if (userEmail === '' || !exRegEmail.test(userEmail)) {
+      console.log('email invalido');
+    } else if (userPassword === '' || userPassword.length < 6) {
+      console.log('contraseña invalida');
+    } else {
+      fetch('http://localhost:3002/users', {
+        method: 'POST',
+        body: JSON.stringify(user),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+        .then((resp) => resp.json())
+        .catch((error) => console.log(error))
+        .then((resp) => console.log(resp));
+    }
+    setUser({...initialState});
+  }
 
   return (
     <>
       <section className="bg-img-fries">
         <div className="modal-window">
           <p className="title-modal">Agregar usuario</p>
-          <FormUsers handleInputChange={handleInputChange} handleSelectChange={handleSelectChange} handleSave={handleSave}/>
+          <FormUsers user={user} handleInputChange={handleInputChange} handleSelectChange={handleSelectChange} handleCancel={handleCancel} handleSave={handleSave}/>
         </div>
       </section>
     </>
