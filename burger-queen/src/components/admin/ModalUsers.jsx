8e901379b23b16e8c66279f367a14234e84dc303
mariebec <jmariebc@gmail.com>
2020-07-14 11:03:55 -0500

    Agregamos la funcionalidad para editar info del usuario

--- burger-queen/src/components/admin/ModalUsers.jsx
+++ burger-queen/src/components/admin/ModalUsers.jsx
@@ -1,14 +1,15 @@
 import React, {useState} from 'react';
 import ReactDOM from 'react-dom'
 import FormUsers from './FormUsers';
-import { postUser } from '../../controller/admin-users';
+import { postUser, putUser } from '../../controller/admin-users';
 
 
 // Obtenemos el estado de display y la función closeModal
-const ModalUsers = ({display, closeModal, setUsers, users, setUser, user}) => {
+const ModalUsers = ({display, setDisplay, setAllUsers, allUsers, setUser, user, button}) => {
   
   const [ errMail, setErrorMail ] = useState(false);
   const [ errPass, setErrorPass ] = useState(false);
+  const [ error, setError ] = useState('');
 
   const handleInputChange = (e) => {
     setUser({...user, [e.target.name]: e.target.value});
@@ -20,11 +21,12 @@ const ModalUsers = ({display, closeModal, setUsers, users, setUser, user}) => {
   // Creamos la función handleCancel que se pasa al formulario
   const handleCancel = () => { 
     // Llamamos la función que tiene el setDisplay(false)
-    closeModal();
-    const idGenerado = (Math.random() * 100).toString();
+    setDisplay(false);
+    const idGenerado = (Math.random() * 1000).toFixed(3).toString();
     setUser({id: idGenerado, email: '', password: '', roles: {admin: false}});
     setErrorMail(false);
     setErrorPass(false);
+    setError('');
   }
 
   const handleSave = () => {
@@ -39,11 +41,24 @@ const ModalUsers = ({display, closeModal, setUsers, users, setUser, user}) => {
     } else { 
       postUser(user)
         .catch((error) => console.log(error))
-        .then((resp) => console.log(resp) || setUsers([...users, resp])); 
-      const idGenerado = (Math.random() * 100).toString();
+        .then((resp) => setAllUsers([...allUsers, resp])); 
+      const idGenerado = (Math.random() * 1000).toFixed(3).toString();
       setUser({id: idGenerado, email: '', password: '', roles: {admin: false}});
-      closeModal();
+      setDisplay(false);
     }
+  }
+
+  const handleEdit = () => {
+    putUser(user).then((resp) => {
+      setDisplay(false);
+      setError('');
+      const idGenerado = (Math.random() * 1000).toFixed(3).toString();
+      setUser({id: idGenerado, email: '', password: '', roles: {admin: false}});
+      setAllUsers(allUsers.map((user) => user.id === resp.id? resp : user));
+    })
+    .catch((error) => {
+      setError(error)
+    });
   }
 
   if(display) {
@@ -56,11 +71,13 @@ const ModalUsers = ({display, closeModal, setUsers, users, setUser, user}) => {
             user={user} 
             errMail={errMail}
             errPass={errPass}
+            error={error}
             handleInputChange={handleInputChange} 
             handleSelectChange={handleSelectChange} 
             handleSave={handleSave}
-            /* Pasamos como prop el handleCancel */
-            handleCancel={handleCancel}/>
+            handleEdit={handleEdit}
+            handleCancel={handleCancel}
+            button={button}/>
         </div>
       </section>, document.getElementById("modal")
     )
