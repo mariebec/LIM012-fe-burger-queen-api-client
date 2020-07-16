0b486eb68af9b114c4c4baf2f9d61927d2923c9a
mariebec <jmariebc@gmail.com>
2020-07-16 17:41:12 -0500

    Cambiando nombre de campos

--- burger-queen/src/components/admin/products/TempFormProducts.jsx
+++ burger-queen/src/components/admin/products/TempFormProducts.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const FormUsers = ({ user, errMail, errPass, error, handleInputChange, handleSelectChange, handleSave, handleEdit, handleCancel, button }) => {
+const FormUsers = ({ product, error, handleInputChange, handleSave, handleEdit, handleCancel, display }) => {
   return (
     <form className="form-modal">
       <div className="form-container">
@@ -8,51 +8,51 @@ const FormUsers = ({ user, errMail, errPass, error, handleInputChange, handleSel
           <label htmlFor="input-name" className="label-text">Nombre:</label>
           <div className="box-input">
             <input 
-              value={user.email}
+              value={product.name}
               id ="input-name" 
               placeholder="Ingrese el nombre" 
-              name="email"
-              className="input-modal"
-              onChange={handleInputChange} />
-              {errMail && <span>e-mail inválido</span>} 
+              name="name"
+              type="text"
+              onChange={handleInputChange} 
+              className={ error.name ? "input-modal error" : "input-modal" } />
           </div>
         </div>
         <div className="field-p">
           <label htmlFor="input-price" className="label-text">Precio:</label>
+          <p className="label-text">S/.</p>
           <div className="box-input">
             <input 
-              value={user.password}
+              value={product.price}
               id ="input-price" 
               placeholder="Ingrese el precio" 
               name="price"
-              className="input-modal" 
-              onChange={handleInputChange} />
-              {errPass && <span>contraseña inválida</span>} 
+              onChange={handleInputChange}
+              type="text"
+              className="input-modal"/>
+              {error.price && <span>contraseña inválida</span>} 
           </div>
         </div>
         <div className="field-p">
           <label htmlFor="input-category" className="label-text">Categoría:</label>
-          <div className="box-input">
-            <input 
-              value={user.password}
-              id ="input-category" 
-              placeholder="Ingrese la categoría" 
-              name="category"
-              className="input-modal" 
-              onChange={handleInputChange} />
-              {errPass && <span>contraseña inválida</span>} 
+          <div className="box-option">
+            <select id ="input-admin" onChange={handleInputChange} name="type" className="select-modal" value={product.type}>
+              <option value="breakfast">Desayuno</option>
+              <option value="burger">Hamburguesa</option>
+              <option value="extra">Adicional</option>
+              <option value="drink">Bebida</option>
+            </select>
           </div>
         </div>
         <div className="field-p">
           <label htmlFor="input-date" className="label-text">Fecha:</label>
           <div className="box-option">
             <input 
-              value={user.password}
+              value={product.date}
               id ="input-date" 
               type="date"
               name="date"
               onChange={handleInputChange} />
-              {errPass && <span>contraseña inválida</span>} 
+              {error.password && <span>contraseña inválida</span>} 
           </div>
         </div>
         <div className="field-p">
@@ -64,7 +64,7 @@ const FormUsers = ({ user, errMail, errPass, error, handleInputChange, handleSel
         {error.message !== '' && <span>{error.message}</span>}
         <div>
           <button type="button" className="btn-modal cancel" onClick={handleCancel}>Cancelar</button>
-          {button ? (
+          {display.button ? (
             <button type="button" className="btn-modal save" onClick={handleEdit}>Editar</button>
           ) : (
             <button type="button" className="btn-modal save" onClick={handleSave}>Guardar</button>
