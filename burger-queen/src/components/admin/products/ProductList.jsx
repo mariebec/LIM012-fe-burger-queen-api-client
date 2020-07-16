41b7c6ffede131e1b6bd1f7e0f6226388208dde3
mariebec <jmariebc@gmail.com>
2020-07-16 17:40:44 -0500

    Simplificando estados de display. Cambiando nombre de estados de user a product

--- burger-queen/src/components/admin/products/ProductList.jsx
+++ burger-queen/src/components/admin/products/ProductList.jsx
@@ -1,47 +1,53 @@
 import React, {useState, useEffect} from 'react';
 import Header from '../../Header';
 import UsersTable from './TempProductsTable';
-import { getUsers, deleteUser } from '../../../controller/admin-users';
+import { getUsers, deleteUser } from '../../../controller/admin-products';
 import ModalUsers from './ModalProduct';
 
 
 const UserList = () => {
 
-  const [ allUsers, setAllUsers ] = useState([]);
+  const idGenerado = (Math.random() * 100).toFixed(2).toString();
+  const initialState = {
+    id: idGenerado,
+    name:'',
+    price: '',
+    image: '',
+    type: 'breakfast',
+    date: '',
+  };
+
+  const [ product, setProduct ] = useState(initialState);
+  const [ allProducts, setAllProducts ] = useState([]);
+  const [ display, setDisplay ] = useState([{
+    modal: false,
+    button: false
+  }]);
 
   useEffect(() => {
-    getUsers().then((resp) => setAllUsers(resp));
+    getUsers().then((resp) => setAllProducts(resp));
   }, []);
 
-  // Acá se crea el estado de display
-  const [display, setDisplay] = useState(false);
-  const [ button, setButton ] = useState(false);
-  // El botón agregar usuario cambia el estado de display a true
   const handleAddUser = () => {
-    setDisplay(true);
-    setButton(false);
+    setDisplay({ modal: true, button: false });
   }
 
   const handleDeleteUser = (id) => {
-    console.log('se va a borrar', id)
-    setAllUsers(allUsers.filter((user) => user.id !== id))
-    deleteUser(id).then((resp) => console.log(resp));
+    // console.log('se va a borrar', id)
+    setAllProducts(allProducts.filter((user) => user.id !== id))
+    deleteUser(id);
   }
 
-  const idGenerado = (Math.random() * 1000).toFixed(3).toString();
-  const initialState = {
-    id: idGenerado,
-    email:'',
-    password:'',
-    roles: { admin: false }
-  };
-
-  const [user, setUser] = useState(initialState);
-
-  const handleUpdateUser = (user) => {
-    setDisplay(true);
-    setButton(true);
-    setUser({...user, id: user.id, email: user.email, roles: {admin: user.roles.admin}});
+  const handleUpdateUser = (product) => {
+    setDisplay({ modal: true, button: true });
+    setProduct({ 
+      id: product.id, 
+      name: product.name, 
+      price: product.price, 
+      image: product.image, 
+      type: product.type, 
+      date: product.date
+    });
   }
 
   return (
@@ -50,12 +56,11 @@ const UserList = () => {
       <main className="container-users">
         <div className="btn-container">
           <button onClick={handleAddUser}>Agregar producto</button>
-          {/* Acá pasamos el estado de display y la función para cambiar el estado a false */}
-          <ModalUsers display={display} setDisplay={setDisplay} setAllUsers={setAllUsers} 
-          allUsers={allUsers} setUser={setUser} user={user} button={button}/>
+          <ModalUsers display={display} setDisplay={setDisplay} setAllProducts={setAllProducts} 
+          allProducts={allProducts} setProduct={setProduct} product={product}/>
         </div>
         <h2>Lista de productos</h2>
-        <UsersTable allUsers={allUsers} handleDeleteUser={handleDeleteUser} handleUpdateUser={handleUpdateUser}/>
+        <UsersTable allProducts={allProducts} handleDeleteUser={handleDeleteUser} handleUpdateUser={handleUpdateUser}/>
 
       </main>
     </>
