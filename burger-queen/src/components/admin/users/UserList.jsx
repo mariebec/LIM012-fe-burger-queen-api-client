ff1c0ff015976aa274400661df10d1e5d3510e6e
Lourdes0896 <sam.lourdesmerino@gmail.com>
2020-07-17 09:23:28 -0500

    unificando estados para el display

--- burger-queen/src/components/admin/users/UserList.jsx
+++ burger-queen/src/components/admin/users/UserList.jsx
@@ -7,27 +7,6 @@ import ModalUsers from './ModalUsers';
 
 const UserList = () => {
 
-  const [ allUsers, setAllUsers ] = useState([]);
-
-  useEffect(() => {
-    getUsers().then((resp) => setAllUsers(resp));
-  }, []);
-
-  // Acá se crea el estado de display
-  const [display, setDisplay] = useState(false);
-  const [ button, setButton ] = useState(false);
-  // El botón agregar usuario cambia el estado de display a true
-  const handleAddUser = () => {
-    setDisplay(true);
-    setButton(false);
-  }
-
-  const handleDeleteUser = (id) => {
-    console.log('se va a borrar', id)
-    setAllUsers(allUsers.filter((user) => user.id !== id))
-    deleteUser(id).then((resp) => console.log(resp));
-  }
-
   const idGenerado = (Math.random() * 1000).toFixed(3).toString();
   const initialState = {
     id: idGenerado,
@@ -35,13 +14,38 @@ const UserList = () => {
     password:'',
     roles: { admin: false }
   };
-
+ 
   const [user, setUser] = useState(initialState);
+  const [allUsers, setAllUsers ] = useState([]);
+  const [display, setDisplay] = useState([{
+    modal: false,
+    button: false
+  }]);
+
+  useEffect(() => {
+    getUsers().then((resp) => setAllUsers(resp));
+  }, []);
+
+  // El botón agregar usuario cambia el estado de display a true
+  const handleAddUser = () => {
+    setDisplay({modal: true, button: false})
+  }
+
+  const handleDeleteUser = (id) => {
+    // console.log('se va a borrar', id)
+    setAllUsers(allUsers.filter((user) => user.id !== id))
+    deleteUser(id).then((resp) => console.log(resp));
+  }
+
+ 
 
   const handleUpdateUser = (user) => {
-    setDisplay(true);
-    setButton(true);
-    setUser({...user, id: user.id, email: user.email, roles: {admin: user.roles.admin}});
+    setDisplay({modal: true, button: true});
+    setUser({id: user.id,
+       email: user.email, 
+       password: user.password,
+       roles: {admin: user.roles.admin}
+      });
   }
 
   return (
@@ -52,7 +56,7 @@ const UserList = () => {
           <button onClick={handleAddUser}>Agregar usuario</button>
           {/* Acá pasamos el estado de display y la función para cambiar el estado a false */}
           <ModalUsers display={display} setDisplay={setDisplay} setAllUsers={setAllUsers} 
-          allUsers={allUsers} setUser={setUser} user={user} button={button}/>
+          allUsers={allUsers} setUser={setUser} user={user}/>
         </div>
         <h2>Lista de usuarios</h2>
         <UsersTable allUsers={allUsers} handleDeleteUser={handleDeleteUser} handleUpdateUser={handleUpdateUser}/>
