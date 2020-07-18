d6e164b25f00c903b57588e9655496628749bed0
Lourdes0896 <sam.lourdesmerino@gmail.com>
2020-07-18 17:23:13 -0500

    agregando test en header.test

--- burger-queen/src/tests/header.test.js
+++ burger-queen/src/tests/header.test.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Header from '../components/Header';
 
 describe('<Header/>', () => {
+
   test('Debería retornar el Header con el título Administrador', () => {
     const title = 'Administrador';
     const { getByText } = render(<Header title={title}/>);
-
     expect(getByText(title)).toBeInTheDocument();
   })
+
+  test('Debería encontrar "Lista de usuarios" en el componente', () => {
+    render(<Header />);
+    expect(screen.getByText('Pedro Campbell')).toBeInTheDocument();
+  });
 })
