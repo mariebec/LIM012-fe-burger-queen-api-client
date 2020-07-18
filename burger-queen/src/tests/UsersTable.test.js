9a15dda4266fbdf2f0d5346b3f68f9eb12a2417c
Lourdes0896 <sam.lourdesmerino@gmail.com>
2020-07-18 17:25:11 -0500

    agregando test para usertable

--- burger-queen/src/tests/UsersTable.test.js
+++ burger-queen/src/tests/UsersTable.test.js
@@ -1,3 +1,15 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import UsersTable from '../components/admin/products/TempProductsTable';
 
+describe('App', () => {
+  test('verificar que no existe un elemento', () => {
+    render(<UsersTable />);
+ 
+    screen.debug();
+ 
+    // fails
+    expect(screen.queryByText(/Searches for JavaScript/)).toBeNull();
+    console.log(renderResult)
+  });
+});
