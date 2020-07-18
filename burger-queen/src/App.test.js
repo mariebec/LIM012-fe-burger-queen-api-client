5320e0a3a03f0562697e8407336d8f861709e7c4
Lourdes0896 <sam.lourdesmerino@gmail.com>
2020-07-18 17:22:11 -0500

    gregando test de componentes

--- burger-queen/src/App.test.js
+++ burger-queen/src/App.test.js
@@ -1,9 +1,44 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
+import Header from './components/Header';
+import LogIn from './components/login/LogIn';
+import Menu from './components/order/Menu';
+import UserList from './components/admin/users/UserList';
+import ProductList from './components/admin/products/ProductList';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+describe ('App', () => {
+  test('Deberia encontrar el componente App', () => {
+    render (<App/>);
+    screen.debug();
+  });
+
+  test('Deberia encontrar el componente Header', () => {
+    render (<Header/>);
+    screen.debug();
+  });
+
+  test('Deberia encontrar el componente de Login', () => {
+    render (<LogIn/>);
+    screen.debug();
+  });
+
+  test('Debería encontrar el componente menu', () => {
+    render (<Menu/>);
+    screen.debug();
+  });
+
+  test('Debería encontrar el componente userList', () => {
+    render (<UserList/>);
+    screen.debug();
+  });
+  test('Debería encontrar el componente ProductList', () => {
+    render (<ProductList/>);
+    screen.debug();
+  });
+
+
+
+
 });
