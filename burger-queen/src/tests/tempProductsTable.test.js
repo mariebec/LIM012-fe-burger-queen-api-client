10cfa7e5a8c47fbd22f77182159290d472d15807
mariebec <jmariebc@gmail.com>
2020-07-19 16:45:48 -0500

    Testeando tempProductosTable
    Verificando si se renderizan los nombres de los productos del array y que no se renderice el texto por defecto cuando no hay productos

---
+++ burger-queen/src/tests/tempProductsTable.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import TempProductsTable from '../components/admin/products/TempProductsTable';
+
+
+const allProducts = [
+  {
+    id: 'u_01',
+    name:'Hamburguesa',
+    price: '10',
+    image: 'URL',
+    type: 'burger',
+    date: '20/09/2020',
+  },
+  {
+    id: 'u_02',
+    name:'Sandwich',
+    price: '10',
+    image: 'URL',
+    type: 'breakfast',
+    date: '20/09/2020',
+  },
+];
+
+describe('TempProductsTable' , () => {
+  test('Debería mostrar si el nombre del producto está en el documento', () => {
+
+    render(<TempProductsTable allProducts={allProducts}/>);
+
+    expect(screen.getByText('Hamburguesa')).toBeInTheDocument();
+    expect(screen.getByText('Sandwich')).toBeInTheDocument();
+    expect(screen.queryByText('No hay productos agregados')).not.toBeInTheDocument();
+  });
+})
