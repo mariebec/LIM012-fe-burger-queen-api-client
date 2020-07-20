bda81b73ebdadb308cae7a9a4ca9eeb6893e1faf
mariebec <jmariebc@gmail.com>
2020-07-20 10:14:11 -0500

    Agregando estado de del select antes del testeo del evento

--- burger-queen/src/tests/formUsers.test.js
+++ burger-queen/src/tests/formUsers.test.js
@@ -53,6 +53,7 @@ describe('Eventos', () => {
 
     render(<FormUsers user={user} error={error} display={display}/>);
 
+    expect(screen.getByText('NO').selected).toBe(false);
     userEvent.selectOptions(screen.getByRole('combobox'), 'NO');
     expect(screen.getByText('NO').selected).toBe(true);
   });
