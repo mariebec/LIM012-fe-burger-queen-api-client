509151df2c761df846fdd742be30ec5a089d8082
mariebec <jmariebc@gmail.com>
2020-07-20 10:14:46 -0500

    Testeando el click que cambia el display del modal

--- burger-queen/src/tests/UserList.test.js
+++ burger-queen/src/tests/UserList.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import UserList from '../components/admin/users/UserList';
+import userEvent from '@testing-library/user-event';
 
-describe('FormUsers', () => {
-
+describe('Render', () => {
   test('Debería encontrar "Agregar usuarios" en el componente', () => {
     render (<UserList/>);
     screen.getByText('Agregar usuario');
@@ -20,5 +20,14 @@ describe('FormUsers', () => {
     // getByRole busca un elemento según su tipo
     expect(screen.getByRole('button')).toBeInTheDocument();
   });
+});
 
+describe('click', () => {
+  test('Debería cambiar el estado de Display modal', () => {
+    render(<UserList/>);
+
+    expect(screen.queryByRole('combobox')).not.toBeInTheDocument();
+    userEvent.click(screen.getByRole('button'));
+    expect(screen.queryByRole('combobox')).toBeInTheDocument();
+  });
 });
