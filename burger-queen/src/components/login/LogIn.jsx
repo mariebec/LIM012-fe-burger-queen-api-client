287ffef47e8b2637dbf6c07b1b069fff43f0a941
Lourdes0896 <sam.lourdesmerino@gmail.com>
2020-07-22 10:47:38 -0500

    intentando obtener token

--- burger-queen/src/components/login/LogIn.jsx
+++ burger-queen/src/components/login/LogIn.jsx
@@ -1,29 +1,45 @@
 import React, {useState} from 'react';
 import img from '../../assets/logo-fondoblanco.png';
 import FormLogin from '../login/FormLogin';
+import {postAuth } from '../../controller/auth';
 
 const LoginView = () => {
 
   const [ user, setUser ] = useState({
     userEmail: '',
-    userContraseña: ''
+    userContraseña: '',
+    login: false
   }); 
+
+  const [ error, setError ] = useState({
+    userEmail: false,
+    userContraseña: false,
+  });
 
   const handleChangeUser = (e) => {
     setUser({...user, [e.target.name]: e.target.value});
   }
 
   const handleGetLogin = () => {
-    let account = { user}
-    if (account) {
-      console.log ('account', account)
+    // let account = { user}
+    if (user) {
+      postAuth(user).then((resp) => {
+        console.warm('resp', resp)
+        localStorage.setItem('login', JSON.stringify ({
+          login:true,
+          token:resp.token
+        })) 
+    }).catch((error) => {
+      setError(error) 
+      .setUser({login:true})
+    });
     }
   }
 
   return(
     <section className="container-login">
     <div className="box-login">
-      <img src={img} alt="logo" className="logo" />
+      <img src={img} alt="logo" className="logo" /> 
       <FormLogin
       handleChangeUser={handleChangeUser}
       handleGetLogin={handleGetLogin}/>  
