be246bb16e867b99ec8f5712aaac54d6e46bc9ee
mariebec <jmariebc@gmail.com>
2020-07-22 18:13:23 -0500

    Agregando menu nav

--- burger-queen/src/components/Header.jsx
+++ burger-queen/src/components/Header.jsx
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import img from '../assets/logo-fondomarron.png';
 
-const Header = (props) => {
+const Header = ({ title, userName, admin }) => {
+  const [ display, setDisplay ] = useState(false);
+  
   return (
     <header className="header-menu">
-      <img src={img} alt="logo" className="logo-Header" />
-      <p className="title">{props.title}</p>
-      <div className="user-logged">
-        <p className="user-name">Pedro Campbell</p>
-        <i className="user-icon fas fa-user"></i>
-        <i className="menu-icon fas fa-bars"></i>
+      <div className="principal-menu">
+        <img src={img} alt="logo" className="logo-Header" />
+        <p className="title">{title}</p>
+        <div className="user-logged">
+          <p className="user-name">{userName}</p>
+          <i className="user-icon fas fa-user"></i>
+          <i className="menu-icon fas fa-bars" onClick={() => display ? setDisplay(false) : setDisplay(true)}></i>
+        </div>
       </div>
+      { display && 
+      <nav className="nav">
+        <ul>
+          <li>Pedidos listos</li>
+          <li>Cocina</li>
+          {admin && 
+          <>
+            <li>Administrar usuarios</li>
+            <li>Administrar productos</li>
+          </>}
+          
+        </ul>
+      </nav>
+      }
+      
     </header>
   )
 }
