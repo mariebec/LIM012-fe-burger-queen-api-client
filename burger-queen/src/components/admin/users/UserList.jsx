a9e6171b05637be5ff60c2c360604762569a5821
mariebec <jmariebc@gmail.com>
2020-07-26 22:11:19 -0500

    Se unificaron todos los estados en uno solo
    Las funciones se ubicaron en el componente que se debe usar

--- burger-queen/src/components/admin/users/UserList.jsx
+++ burger-queen/src/components/admin/users/UserList.jsx
@@ -1,52 +1,46 @@
 import React, {useState, useEffect} from 'react';
 import Header from '../../Header';
 import UsersTable from './UsersTable';
-import { getUsers, deleteUser } from '../../../controller/admin-users';
+import { getUsers } from '../../../controller/admin-users';
 import ModalUsers from './ModalUsers';
 
 
 const UserList = () => {
 
   const idGenerado = (Math.random() * 1000).toFixed(3).toString();
-  const initialState = {
-    id: idGenerado,
-    email:'',
-    password:'',
-    roles: { admin: false }
-  };
- 
-  const [user, setUser] = useState(initialState);
-  const [allUsers, setAllUsers] = useState([]);
-  const [display, setDisplay] = useState({
-    modal: false,
-    button: false
+  const [ state, setState ] = useState({
+    allUsers: [],
+    userData: {
+      id: idGenerado,
+      email:'',
+      password:'',
+      roles: { admin: false }
+    },
+    display: {
+      modal: false,
+      btnEdit: false
+    }
   });
 
   useEffect(() => {
-    getUsers().then((resp) => setAllUsers(resp));
+    getUsers().then((resp) => setState(prevState => ({
+      ...prevState,
+      allUsers: resp
+    })));
   }, []);
-
-  const handleDeleteUser = (id) => {
-    setAllUsers(allUsers.filter((user) => user.id !== id));
-    deleteUser(id);
-  };
 
   const modalRoot = document.createElement('div');
   modalRoot.setAttribute('id', 'modal');
 
-  const handleUpdateUser = (user) => {
-    setDisplay({modal: true, button: true});
-    document.body.append(modalRoot);
-    setUser({id: user.id,
-       email: user.email, 
-       password: user.password,
-       roles: {admin: user.roles.admin}
-      });
-  };
-
   const handleAdd = () => {
     document.body.append(modalRoot);
-    setDisplay({ modal: true, button: false })
+    setState(prevState => ({ 
+      ...prevState,
+      display: {
+        modal: true,
+        btnEdit: false
+      }
+    }));
   };
 
   return (
@@ -55,12 +49,10 @@ const UserList = () => {
       <main className="container-list">
         <div className="btn-container">
           <button onClick={handleAdd}>Agregar usuario</button>
-          {/* Acá pasamos el estado de display y la función para cambiar el estado a false */}
-          <ModalUsers display={display} setDisplay={setDisplay} setAllUsers={setAllUsers} 
-          allUsers={allUsers} setUser={setUser} user={user}/>
+          <ModalUsers state={state} setState={setState}/>
         </div>
         <h2>Lista de usuarios</h2>
-        <UsersTable allUsers={allUsers} handleDeleteUser={handleDeleteUser} handleUpdateUser={handleUpdateUser}/>
+        <UsersTable state={state} setState={setState} modalRoot={modalRoot}/>
 
       </main>
     </>
