1126e90f0e03f419567a69a638565cfc6691d5e7
Lourdes0896 <sam.lourdesmerino@gmail.com>
2020-07-28 18:51:59 -0500

    Âagregando validaciones del login y errores

--- burger-queen/src/components/login/LogIn.jsx
+++ burger-queen/src/components/login/LogIn.jsx
@@ -12,31 +12,47 @@ const LoginView = () => {
     userContraseña: '',
   }); 
 
-  const [ error, setError ] = useState('');
+  const [ error, setError ] = useState({
+    userEmail: false,
+    userContraseña: false
+  });
 
   const handleChangeUser = (e) => {
     setUser({...user, [e.target.name]: e.target.value});
   }
 
-  const handleGetLogin = () => {
+  const handleGetLogin = (request) => {
     const email = user.userEmail;
     const password = user.userContraseña
-    postAuth({email, password}).then((resp) => {
-      history.push("/categories");
-      sessionStorage.setItem('login', resp.token);
-      getUserByEmail(email).then((user) => {
-        sessionStorage.setItem('currentRol', user.roles.admin);
-        sessionStorage.setItem('currentEmail', user.email);
-      })
-    }).catch((err) => {
-      setError(err); 
-    });
+
+    const exRegEmail = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/;
+    const notValidEmail = email.trim() === '' || !exRegEmail.test(email.trim());
+    const notValidPassword = password.trim() === '' || password.trim().length < 6;
+
+    if (notValidEmail || notValidPassword) {
+      (notValidEmail) ? setError(prevState => ({ ...prevState, userEmail: true })) : setError(prevState => ({ ...prevState, userEmail: false }));
+      (notValidPassword) ? setError(prevState => ({ ...prevState, userContraseña: true })) : setError(prevState => ({ ...prevState, userContraseña: false }));
+      
+    } else{
+      postAuth({email, password}).then((resp) => {
+        history.push("/categories");
+        sessionStorage.setItem('login', resp.token);
+        getUserByEmail(email).then((user) => {
+          sessionStorage.setItem('currentRol', user.roles.admin);
+          sessionStorage.setItem('currentEmail', user.email);
+        })
+      }).catch((err) => {
+        setError(err); 
+      });
+      
+    }
   }
   return(
     <section className="container-login">
     <div className="box-login">
       <img src={img} alt="logo" className="logo" /> 
       <FormLogin
+      user={user}
       error={error}
       handleChangeUser={handleChangeUser}
       handleGetLogin={handleGetLogin}/>  
