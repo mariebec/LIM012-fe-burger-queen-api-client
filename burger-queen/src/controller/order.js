4b905993b7ff56d81193a7cf7c7252816763b21f
mariebec <jmariebc@gmail.com>
2020-08-04 18:03:46 -0500

    Creando archivo para post de orders

---
+++ burger-queen/src/controller/order.js
@@ -0,0 +1,10 @@
+export default (order) => {
+  console.log(order);
+//   fetch('http://localhost:3000/orders', {
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/json',
+//     // Authorization: `Bearer ${localStorage.getItem('token')}`,
+//   },
+// }).then((resp) => resp.json());
+};
