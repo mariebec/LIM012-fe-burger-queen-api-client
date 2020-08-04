cf8548c1473909754036ff02dd49a0675ef7c76f
Lourdes0896 <sam.lourdesmerino@gmail.com>
2020-08-04 16:46:48 -0500

    cambiando test de formulario de users y productos

--- burger-queen/src/tests/components/admin-product/tempFormProducts.test.js
+++ burger-queen/src/tests/components/admin-product/tempFormProducts.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import TempFormProducts from '../../../components/admin/products/TempFormProducts';
 
+const setProduct = jest.fn();
 const product = {
   productData: {
     id: '1',
@@ -10,21 +11,20 @@ const product = {
     price: '15',
     image: 'some URL',
     type: 'burger',
-    date: '2020-07-15'
+    date: '2020-07-15',
   },
   display: {
     modal: false,
-    button: true
-  }
-  
-}
+    button: true,
+  },
+};
+
 const error = {
   name: false,
   price: false,
   date: false,
-  api: ''
+  api: '',
 };
-
 
 describe('Render', () => {
   test('Debería retornar el valor de name', () => {
@@ -32,7 +32,7 @@ describe('Render', () => {
     expect(product.productData.name).toBe('Hamburguesa doble');
   });
   test('Debería mostrar el botón de Editar', () => {
-    render(<TempFormProducts product={product} error={error}/>);
+    render(<TempFormProducts product={product} error={error} />);
     expect(screen.getByText('Editar')).toBeInTheDocument();
   });
 
@@ -45,26 +45,22 @@ describe('Render', () => {
     render(<TempFormProducts product={product} error={error} />);
     expect(screen.queryByDisplayValue('Hamburguesa')).toBeInTheDocument();
   });
-
-  
 });
-
 
 describe('Eventos', () => {
   test('Debería cambiar el value del select', () => {
-
-    render(<TempFormProducts product={product} error={error} />);
+    render(<TempFormProducts product={product} setProduct={setProduct} />);
 
     userEvent.selectOptions(screen.getByRole('combobox'), 'drink');
     expect(screen.getByText('Bebida').selected).toBe(true);
   });
 
   test('Debería llamar al evento handleInputChange la cantidad de veces que se tipea', async () => {
-    const handleInputChange = jest.fn();
+    // const handleInputChange = jest.fn();
 
-    render(<TempFormProducts product={product} error={error}  handleInputChange={handleInputChange}/>);
-
-    await userEvent.type(screen.getAllByRole('textbox'), 'hamburguesa'); 
-    expect(handleInputChange).toHaveBeenCalledTimes(11);
+    render(<TempFormProducts product={product} setProduct={setProduct} />);
+    await fireEvent.change(screen.getByRole('textbox'),
+      { target: { value: 'Hamburguesa' } });
+    expect(screen.getByText('Hamburguesa')).toBeInTheDocument();
   });
 });
