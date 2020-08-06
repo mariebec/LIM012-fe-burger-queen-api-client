5c8bcd16fa6d6ca376bb01074683f3cf749f1cb3
mariebec <jmariebc@gmail.com>
2020-08-06 09:55:00 -0500

    Agregando petición POST

--- burger-queen/src/controller/order.js
+++ burger-queen/src/controller/order.js
@@ -1,10 +1,18 @@
-export default (order) => {
-  console.log(order);
-//   fetch('http://localhost:3000/orders', {
-//   method: 'POST',
-//   headers: {
-//     'Content-Type': 'application/json',
-//     // Authorization: `Bearer ${localStorage.getItem('token')}`,
-//   },
-// }).then((resp) => resp.json());
-};
+/* eslint-disable consistent-return */
+export default (order) => fetch('http://localhost:8000/orders', {
+  method: 'POST',
+  body: JSON.stringify(order),
+  // mode: 'no-cors',
+  headers: {
+    'Content-Type': 'application/json',
+    // 'Authorization': 'Bearer ' + localStorage.getItem('token')
+  },
+}).then((resp) => {
+  if (resp.status === 200) {
+    return resp.json();
+  } if (resp.status === 400) {
+    return new Error('userId o productos son requeridos');
+  } if (resp.status === 401) {
+    return new Error('No hay cabecera de autenticación');
+  }
+});
