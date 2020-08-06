fe7e7b1aa18566afc41c0b17cffbe48b6a4cdfb2
mariebec <jmariebc@gmail.com>
2020-08-06 17:18:33 -0500

    Obteniendo órdenes y mostrando en el cuadro correspondiente según el status "delivering" o "delivered"

--- burger-queen/src/components/order/OrderReady.jsx
+++ burger-queen/src/components/order/OrderReady.jsx
@@ -1,130 +1,109 @@
-import React from 'react';
+/* eslint-disable no-underscore-dangle */
+import React, { useState, useEffect } from 'react';
+import { getOrders } from '../../controller/order';
 import Header from '../Header';
 
-const OrderReady = () => (
-  <>
-    <Header />
-    <section className="container-orderReady">
-      <div className="container-div">
-        <div className="card-scroll">
-          <div className="card-orders">
-            <p className="title">Pedidos para Entregar</p>
-            <div className="list-Order">
-              <div className="header-card-order">
-                <p>Order N° 512-9</p>
-                <p>Rita Holiday</p>
-              </div>
-              <table>
-                <tbody>
-                  <tr>
-                    <td className="number">1</td>
-                    <td>Hamburguesa Doble</td>
-                    <td>s/15</td>
-                  </tr>
-                  <tr>
-                    <td>1</td>
-                    <td>Aros de Cebolla</td>
-                    <td>s/10</td>
-                  </tr>
-                  <tr>
-                    <td>2</td>
-                    <td>Papas fritas</td>
-                    <td>s/5</td>
-                  </tr>
-                  <tr>
-                    <td>1</td>
-                    <td>Hamburguesa Simple</td>
-                    <td>s/15</td>
-                  </tr>
-                </tbody>
-              </table>
-              <div className="foot-card-order">
-                <p>Tiempo: 15.05 min</p>
-                <p>Tiempo: 15.05 min</p>
-              </div>
+const OrderReady = () => {
+  const [delivering, setDelivering] = useState([]);
+  const [delivered, setDelivered] = useState([]);
+
+  useEffect(() => {
+    getOrders().then((resp) => {
+      resp.forEach((order) => {
+        if (order.status === 'delivering') setDelivering((prev) => ([...prev, order]));
+        else if (order.status === 'delivered') setDelivered((prev) => ([...prev, order]));
+      });
+    });
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <section className="container-orderReady">
+        <div className="container-div">
+          <div className="card-scroll">
+            <div className="card-orders">
+              <p className="title">Pedidos para entregar</p>
+              { delivering.length > 0
+                ? delivering.map((order) => (
+                  <div className="list-Order" key={order._id}>
+                    <div className="header-card-order">
+                      <p>
+                        Order N°
+                        {order._id}
+                        {' '}
+                        |
+                        {' '}
+                        {order.client}
+                      </p>
+                      <i className="fas fa-check" />
+                    </div>
+                    <table className="body-card-order">
+                      <tbody>
+                        { order.products.map((item) => (
+                          <tr>
+                            <td className="qty">1</td>
+                            <td>{item.product}</td>
+                            <td className="price">S/. 15</td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
+                    <div className="foot-card-order">
+                      <p>Tiempo: 15.05min</p>
+                      <p className="total">S/. 30</p>
+                    </div>
+                  </div>
+                )) : (
+                  <p>No hay productos para entregar</p>
+                )}
             </div>
           </div>
         </div>
 
-      </div>
-
-      <div className="container-div">
-        <div className="card-scroll">
-          <div className="card-orders">
-            <p className="title">Pedidos Entregados</p>
-            <div className="list-Order">
-              <div className="header-card-order">
-                <p>Order N° 512-9</p>
-                <p>Rita Holiday</p>
-              </div>
-              <table>
-                <tbody>
-                  <tr>
-                    <td className="number">1</td>
-                    <td>Hamburguesa Doble</td>
-                    <td>s/15</td>
-                  </tr>
-                  <tr>
-                    <td>1</td>
-                    <td>Aros de Cebolla</td>
-                    <td>s/10</td>
-                  </tr>
-                  <tr>
-                    <td>2</td>
-                    <td>Papas fritas</td>
-                    <td>s/5</td>
-                  </tr>
-                  <tr>
-                    <td>1</td>
-                    <td>Hamburguesa Simple</td>
-                    <td>s/15</td>
-                  </tr>
-                </tbody>
-              </table>
-              <div className="foot-card-order">
-                <p>Tiempo: 15.05 min</p>
-                <p>Tiempo: 15.05 min</p>
-              </div>
-            </div>
-            <div className="list-Order">
-              <div className="header-card-order">
-                <p>Order N° 512-9</p>
-                <p>Rita Holiday</p>
-              </div>
-              <table>
-                <tbody>
-                  <tr>
-                    <td className="number">1</td>
-                    <td>Hamburguesa Doble</td>
-                    <td>s/15</td>
-                  </tr>
-                  <tr>
-                    <td>1</td>
-                    <td>Aros de Cebolla</td>
-                    <td>s/10</td>
-                  </tr>
-                  <tr>
-                    <td>2</td>
-                    <td>Papas fritas</td>
-                    <td>s/5</td>
-                  </tr>
-                  <tr>
-                    <td>1</td>
-                    <td>Hamburguesa Simple</td>
-                    <td>s/15</td>
-                  </tr>
-                </tbody>
-              </table>
-              <div className="foot-card-order">
-                <p>Tiempo: 15.05 min</p>
-                <p>Tiempo: 15.05 min</p>
-              </div>
+        <div className="container-div">
+          <div className="card-scroll">
+            <div className="card-orders">
+              <p className="title">Pedidos entregados</p>
+              { delivered.length > 0
+                ? delivered.map((order) => (
+                  <div className="list-Order" key={order._id}>
+                    <div className="header-card-order">
+                      <p>
+                        Order N°
+                        {order._id}
+                        {' '}
+                        |
+                        {' '}
+                        {order.client}
+                      </p>
+                      <i className="fas fa-check green" />
+                    </div>
+                    <table className="body-card-order">
+                      <tbody>
+                        { order.products.map((item) => (
+                          <tr>
+                            <td className="qty">1</td>
+                            <td>{item.product}</td>
+                            <td className="price">S/. 15</td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
+                    <div className="foot-card-order">
+                      <p>Tiempo: 15.05min</p>
+                      <p className="total">S/. 30</p>
+                    </div>
+                  </div>
+                )) : (
+                  <p>No hay productos para entregar</p>
+                )}
             </div>
           </div>
         </div>
-      </div>
-    </section>
-  </>
-);
+      </section>
+    </>
+  );
+};
 
 export default OrderReady;
