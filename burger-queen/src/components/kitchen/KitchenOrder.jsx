b811f284a88a10c5513b065391e94427be1771b6
Lourdes0896 <sam.lourdesmerino@gmail.com>
2020-08-07 12:15:12 -0500

    agregando estado a la orden y haciendo peticion Fetch putOrder
    tambien se cambio el estatus de la orden de pending a delivering

--- burger-queen/src/components/kitchen/KitchenOrder.jsx
+++ burger-queen/src/components/kitchen/KitchenOrder.jsx
@@ -2,34 +2,47 @@
 import React, { useState, useEffect } from 'react';
 import Header from '../Header';
 // import OrderList from '../order/orderList';
-import { getOrders } from '../../controller/order';
+import { getOrders, putOrder } from '../../controller/order';
 
 const KitchenOrder = () => {
-  const [kitOrder, setKitOrder] = useState([]);
+  const [pending, setPending] = useState([]);
 
   useEffect(() => {
-    getOrders().then((resp) => setKitOrder(resp));
+    getOrders().then((resp) => {
+      resp.forEach((order) => {
+        if (order.status === 'pending') setPending((prev) => ([...prev, order]));
+      });
+    });
   }, []);
 
-  const handleOrder = () => {
-    setKitOrder((prev) => ({ ...prev, kitOrder: [] }));
+  const handleOrder = (order) => {
+    const arrOrder = pending.find((item) => (item.order === order));
+    if (arrOrder.status === 'pending') {
+      console.log(arrOrder);
+      const obj = {
+        ...arrOrder,
+        status: 'delivering',
+      };
+      console.log(obj);
+      putOrder(obj).then((resp) => console.log(resp));
+    }
+    // setPending(pending.map((order) => (order.status === 'pending'
+    // ? { ...order, status: 'delivering' } : order)));
   };
-  console.log(kitOrder);
 
   return (
     <>
       <Header title="COCINA" />
       <section className="kitchen-container">
         {
-        kitOrder.length > 0
-          ? kitOrder.map((element) => (
+        pending.length > 0
+          ? pending.map((element) => (
             <div className="kitchen-card" key={element.id}>
               <div className="header-card">
                 <p>
                   {element.id}
-                  |
-                  {element.client}
                 </p>
+                <p>{element.client}</p>
                 <p>{element.dateEntry}</p>
               </div>
               <div className="body-card">
@@ -38,13 +51,18 @@ const KitchenOrder = () => {
                   <tbody>
                     {element.products.map((item) => (
                       <tr>
-                        <td>{item.qty}</td>
-                        <td>{item.product}</td>
+                        <td>
+                          {item.qty}
+                        </td>
+                        <td>
+                          {item.product}
+                        </td>
+                        <td type="checkbox"><i className="fas fa-check" /></td>
                       </tr>
                     )) }
                   </tbody>
                 </table>
-                <button type="button" className="order-ready" onClick={handleOrder}> LISTO </button>
+                <button type="button" className="order-ready" onClick={() => handleOrder(element.order)}> LISTO </button>
               </div>
             </div>
           )) : (
