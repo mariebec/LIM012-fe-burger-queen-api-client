e6998bdbe9da15d0cd4e3647e3dd28fa3535d99f
mariebec <jmariebc@gmail.com>
2020-08-10 18:52:10 -0500

    Correcciones eslint

--- burger-queen/src/tests/controller/admin-products.test.js
+++ burger-queen/src/tests/controller/admin-products.test.js
@@ -1,4 +1,6 @@
-import { getProducts, postProduct, deleteProduct, putProduct } from '../../controller/admin-products';
+import {
+  getProducts, postProduct, deleteProduct, putProduct,
+} from '../../controller/admin-products';
 import { server } from '../../__mock__/server';
 
 // Establecer el mock API antes de realizar los tests
@@ -10,45 +12,37 @@ afterAll(() => server.close());
 
 const objProducts = {
   id: 1,
-  name: "Hamburguesa doble",
-  price: "15",
-  image: "", 
-  type: "burger",
-  date: "2020-07-15"
-}
+  name: 'Hamburguesa doble',
+  price: '15',
+  image: '',
+  type: 'burger',
+  date: '2020-07-15',
+};
 
 describe('getProducts', () => {
   test('Debería retornar un producto ', async () => {
-
     const response = await getProducts();
     expect(response).toEqual(objProducts);
-
   });
 });
 
 describe('postProducts', () => {
   test('Debería retornar producto agregado', async () => {
-
     const response = await postProduct(objProducts);
     expect(response).toEqual(objProducts);
-
   });
 });
 
 describe('deleteProduct', () => {
   test('Debería eliminar producto', async () => {
-
     const response = await deleteProduct(1);
     expect(response.message).toBe('El producto ha sido eliminado');
-
   });
 });
 
 describe('putProduct', () => {
   test('Debería retornar producto editado', async () => {
-
     const response = await putProduct(objProducts);
     expect(response).toEqual(objProducts);
-
   });
 });
