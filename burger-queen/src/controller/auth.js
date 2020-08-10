b712dca51c3a788ea8e215bd96aa8704661fb8b4
mariebec <jmariebc@gmail.com>
2020-08-10 18:50:38 -0500

    Probando API

--- burger-queen/src/controller/auth.js
+++ burger-queen/src/controller/auth.js
@@ -2,8 +2,8 @@
 /* eslint-disable consistent-return */
 /* eslint-disable prefer-promise-reject-errors */
 export const postAuth = (user) =>
-  // return fetch('https://burger-queen-apilab.herokuapp.com/auth', {
-  fetch('http://localhost:8000/auth', {
+  fetch('https://burger-queen-apilab.herokuapp.com/auth', {
+  // fetch('http://localhost:8000/auth', {
     method: 'POST',
     body: JSON.stringify(user),
     // mode: 'no-cors',
@@ -12,13 +12,16 @@ export const postAuth = (user) =>
       // 'Authorization': 'Bearer ' + localStorage.getItem('token')
     },
   }).then((resp) => {
-    console.log(resp);
     if (resp.status === 200) {
       return resp.json();
     } if (resp.status === 400) {
       return Promise.reject('El email y password son requeridos');
     }
+    // return (resp);
+  }).catch((err) => {
+    console.log(err);
   });
+
 export const getUserByEmail = (email) =>
   // return fetch(`https://burger-queen-apilab.herokuapp.com/users/${email}`, {
   fetch(`http://localhost:8000/users/${email}`, {
