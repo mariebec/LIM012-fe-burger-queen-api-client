7961935b698c138bbfacb6c9af0f375324f3f819
mariebec <jmariebc@gmail.com>
2020-08-12 22:45:12 -0500

    Cambiando if/else por switch

--- burger-queen/src/controller/order.js
+++ burger-queen/src/controller/order.js
@@ -1,25 +1,24 @@
-/* eslint-disable implicit-arrow-linebreak */
-/* eslint-disable no-underscore-dangle */
 /* eslint-disable consistent-return */
 
-export const postOrder = (order) =>
-  // fetch('http://localhost:8000/orders', {
-  fetch('https://burger-queen-apilab.herokuapp.com/orders', {
-    method: 'POST',
-    body: JSON.stringify(order),
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${sessionStorage.getItem('token')}`,
-    },
-  }).then((resp) => {
-    if (resp.status === 200) {
+export const postOrder = (order) => fetch('https://burger-queen-apilab.herokuapp.com/orders', {
+  method: 'POST',
+  body: JSON.stringify(order),
+  headers: {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${sessionStorage.getItem('token')}`,
+  },
+}).then((resp) => {
+  switch (resp.status) {
+    case 200:
       return resp.json();
-    } if (resp.status === 400) {
+    case 400:
       return new Error('userId o productos son requeridos');
-    } if (resp.status === 401) {
+    case 401:
       return new Error('No hay cabecera de autenticación');
-    }
-  });
+    default:
+      break;
+  }
+});
 
 export const getOrders = () => fetch('https://burger-queen-apilab.herokuapp.com/orders', {
   method: 'GET',
@@ -28,10 +27,13 @@ export const getOrders = () => fetch('https://burger-queen-apilab.herokuapp.com/
     Authorization: `Bearer ${sessionStorage.getItem('token')}`,
   },
 }).then((resp) => {
-  if (resp.status === 200) {
-    return resp.json();
-  } if (resp.status === 401) {
-    return new Error('No hay cabecera de autenticación');
+  switch (resp.status) {
+    case 200:
+      return resp.json();
+    case 401:
+      return new Error('No hay cabecera de autenticación');
+    default:
+      break;
   }
 });
 
@@ -42,4 +44,17 @@ export const putOrder = (order, id) => fetch(`https://burger-queen-apilab.heroku
     'Content-Type': 'application/json',
     Authorization: `Bearer ${sessionStorage.getItem('token')}`,
   },
-}).then((resp) => resp.json());
+}).then((resp) => {
+  switch (resp.status) {
+    case 200:
+      return resp.json();
+    case 400:
+      return new Error('userId o productos son requeridos');
+    case 401:
+      return new Error('No hay cabecera de autenticación');
+    case 404:
+      return new Error('No se encuentra la orden con el ID solicitado');
+    default:
+      break;
+  }
+});
